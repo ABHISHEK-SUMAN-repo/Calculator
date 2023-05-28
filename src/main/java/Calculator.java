d67ad4903a6b242ffd1b36afbfbcca15cfdb4f90
Abhishek1441 <abhishek@kitabeli.id>
2023-05-28 15:07:49 +0530

    final submit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,22 +1,66 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.lang.String.join;
 
 public class Calculator {
 
-    public static int add(String text) {
-        if(text.isEmpty()){
-         return 0;
-        }
-        else if(text.contains(",")){
-            String[] commaSeperated = text.split(",");
-            int[] arr = Arrays.stream(commaSeperated).mapToInt(Integer::parseInt).toArray();
-            return sumofNnumbers(arr);
+    public static int add(String text) throws Exception {
+
+        String[] delimeters = getDelimeters(text);
+        int[] arr = Arrays.stream(delimeters).mapToInt(Integer::parseInt).toArray();
+        negativeNumnersNotAllowed(arr);
+        return sumofNnumbers(arr);
+
 
         }
-        else{
-            return toInt(text);
+
+    private static void negativeNumnersNotAllowed(int[] arr) throws Exception {
+        List<Integer> negative = new ArrayList<>();
+        for(int i = 0; i< arr.length; i++)
+        {
+            if(arr[i]<0)
+            {
+                negative.add(arr[i]);
+            }
+        }
+        if(negative.size()>0)
+        {
+            throw new Exception("negatives_numbers_not_allowed"+join((CharSequence) negative));
         }
     }
+
+    private static String[] getDelimeters(String text) {
+        if(text.isEmpty()){
+            return new String[0];
+        }
+        else if (useCustomDelimeterSyntax(text)) {
+            return splitUsingCustomDelimeter(text);
+        } else {
+            return splitUsingNewLineAndComma(text);
+        }
+    }
+
+    private static boolean useCustomDelimeterSyntax(String text) {
+        return text.startsWith("//");
+    }
+
+    private static String[] splitUsingNewLineAndComma(String text) {
+        String[] token = text.split(",|\n");
+        return token;
+    }
+
+    private static String[] splitUsingCustomDelimeter(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        m.matches();
+        String customDelimeter = m.group(1);
+        String numbers = m.group(2);
+        return numbers.split(Pattern.quote(customDelimeter));
+    }
+
 
     private static int sumofNnumbers(int[] arr) {
         int sum=0;
@@ -28,8 +72,5 @@ public class Calculator {
         return sum;
     }
 
-    private static int toInt(String text) throws NumberFormatException {
-        return Integer.parseInt(text);
-    }
 
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,24 +4,51 @@ import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
     @Test
-    public void returnZeroIfEmptyString() throws Exception{
+    public void returnZeroIfEmptyString() throws Exception {
         int value=Calculator.add("");
         assertEquals(0,value);
     }
     @Test
-    public void returnNumberOneIfOneNumber() throws Exception{
+    public void returnNumberOneIfOneNumber() throws Exception {
         int value=Calculator.add("1");
         assertEquals(1,value);
     }
 
     @Test
-    public void returnSumOfTwoNumbers() throws Exception{
+    public void returnSumOfTwoNumbers() throws Exception {
         int value = Calculator.add("1,2");
         assertEquals(3,value);
     }
     @Test
-    public void returnSumOfMultipleNumbers() throws Exception{
+    public void returnSumOfMultipleNumbers() throws Exception {
         int value = Calculator.add("1,2,3");
         assertEquals(6,value);
     }
+    @Test
+    public void returnForNewLine() throws Exception {
+        int value = Calculator.add("1,2\n3");
+        assertEquals(6,value);
+    }
+    @Test()
+    public void returnValueForCustomDelimeter() throws Exception {
+        int value =Calculator.add("//;\n1;2");
+        assertEquals(3,value);
+    }
+
+    @Test
+    public void ignoreLargeNumbers() throws Exception {
+        int value=Calculator.add("1001,2");
+    }
+
+    @Test
+    public void returnRegex() throws Exception {
+        int value=Calculator.add("//.\n1.2");
+        assertEquals(3,value);
+    }
+
+    @Test(expected = Exception.class)
+    public void returnExceptionOnNegetiveNumbers() throws Exception{
+        Calculator.add("//;\n-1;2");
+    }
+
 }
